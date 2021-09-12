39c04955b6193c6daed2732d20c60bf61870f741
홍순원 <sunwonsw95@naver.com>
2021-09-12 17:05:29 +0900

    테스트 문구 출력

---
+++ lib/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeWidget();
+  }
+}
+
+
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Test Page"),
+    );
+  }
+}
+
