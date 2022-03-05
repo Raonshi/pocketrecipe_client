583fa83c71cb2786b079b7f93cfd2a0a8dc9aa64
홍순원 <hongsun-won@hongsun-won-ui-MacBookAir.local>
2022-03-05 16:35:53 +0900

    refactor recipe search page ui

--- lib/ui/pages/recipe/recipe_search_page.dart
+++ lib/ui/pages/recipe/recipe_search_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pocketrecipe_client/controllers/recipe_search_ctrl.dart';
-import 'package:pocketrecipe_client/getx/controller.dart';
-import 'package:pocketrecipe_client/ui/widgets/recipe_item.dart';
 import 'package:pocketrecipe_client/data_define.dart';
+import 'package:pocketrecipe_client/getx/controller.dart';
+import 'package:pocketrecipe_client/ui/pages/recipe/recipe_page.dart';
 
 class RecipeSearchPage extends StatelessWidget {
   RecipeSearchPage({Key? key}) : super(key: key);
@@ -19,49 +19,7 @@ class RecipeSearchPage extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0, vertical: 8.0),
-                    child: TextField(
-                      controller: controller.searchController,
-                      maxLines: 1,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "요리명 / 요리재료",
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 5.0, horizontal: 10.0),
-                      ),
-                      onSubmitted: (str) {
-                        controller.getRecipeByKeyword();
-                      },
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      controller.getRecipeByKeyword();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15.0),
-                      child: Text(
-                        "검색",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            child: _SearchWidget(),
           ),
           Expanded(
             flex: 8,
@@ -71,7 +29,19 @@ class RecipeSearchPage extends StatelessWidget {
                   scrollDirection: Axis.vertical,
                   itemCount: controller.searchResult.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return RecipeItem(controller.searchResult[index]);
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 10.0,
+                      ),
+                      child: InkWell(
+                        child: _RecipeItemWidget(
+                          recipe: controller.searchResult[index],
+                        ),
+                        onTap: () => Get.to(
+                            () => RecipePage(controller.searchResult[index])),
+                      ),
+                    );
                   }),
             ),
           ),
@@ -81,69 +51,156 @@ class RecipeSearchPage extends StatelessWidget {
   }
 }
 
-// class RecipeSearch extends StatelessWidget {
-//   final controller = Get.put(Controller());
-//   TextEditingController textEditingController = TextEditingController();
+class _SearchWidget extends StatelessWidget {
+  _SearchWidget({Key? key}) : super(key: key);
+  final controller = Get.find<RecipeSearchCtrl>();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     controller.recipeList.clear();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Flexible(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+              child: TextField(
+                controller: controller.searchController,
+                maxLines: 1,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  hintText: "요리명 / 요리재료",
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: 10.0),
+                ),
+                onSubmitted: (str) {
+                  controller.getRecipeByKeyword();
+                },
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.resolveWith(
+                  (states) => RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                controller.getRecipeByKeyword();
+              },
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+                child: Text(
+                  "검색",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-//     return Container(
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Expanded(
-//             flex: 1,
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               mainAxisSize: MainAxisSize.max,
-//               children: [
-//                 Flexible(
-//                   child: Padding(
-//                     padding: const EdgeInsets.symmetric(
-//                         horizontal: 8.0, vertical: 8.0),
-//                     child: TextField(
-//                       controller: textEditingController,
-//                       maxLines: 1,
-//                       decoration: InputDecoration(
-//                           border: OutlineInputBorder(), hintText: "요리명 / 요리재료"),
-//                       onSubmitted: (str) {
-//                         controller
-//                             .getRecipeByKeyword(textEditingController.text);
-//                       },
-//                     ),
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-//                   child: ElevatedButton(
-//                       onPressed: () {
-//                         controller
-//                             .getRecipeByKeyword(textEditingController.text);
-//                       },
-//                       child: Padding(
-//                         padding: const EdgeInsets.symmetric(vertical: 20.0),
-//                         child: Text("검색"),
-//                       )),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Expanded(
-//             flex: 8,
-//             child: Obx(
-//               () => ListView.builder(
-//                   shrinkWrap: true,
-//                   scrollDirection: Axis.vertical,
-//                   itemCount: controller.recipeList.length,
-//                   itemBuilder: (BuildContext context, int index) {
-//                     return RecipeItem(controller.recipeList[index]);
-//                   }),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class _RecipeItemWidget extends StatelessWidget {
+  _RecipeItemWidget({Key? key, required this.recipe}) : super(key: key);
+  final controller = Get.find<RecipeSearchCtrl>();
+  final Recipe recipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.lightBlue,
+          width: 2.0,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //레시피 이미지
+          Expanded(
+            flex: 4,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: recipe.recipeImg == "Unknown"
+                  ? Image.asset(
+                      "data/warning.jpeg",
+                    )
+                  : Image.network("${recipe.recipeImg}"),
+            ),
+          ),
+
+          Spacer(),
+
+          //레시피 이름
+          Expanded(
+            flex: 6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  recipe.name,
+                  maxLines: 1,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Divider(color: Color.fromARGB(0, 0, 0, 0)),
+                Text(
+                  "열량 : ${recipe.energy}kcal",
+                  maxLines: 1,
+                  style: TextStyle(fontSize: 16),
+                ),
+                Divider(color: Color.fromARGB(0, 0, 0, 0)),
+                Text(
+                  "superCheif",
+                  maxLines: 1,
+                  style: TextStyle(fontSize: 16),
+                ),
+                Divider(color: Color.fromARGB(0, 0, 0, 0)),
+                Row(
+                  children: [
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      constraints: BoxConstraints(),
+                      splashRadius: 8,
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    Divider(
+                      color: Color.fromARGB(0, 0, 0, 0),
+                    ),
+                    Text(
+                      "+120k likes",
+                      style: TextStyle(fontSize: 10, color: Colors.grey),
+                    ),
+                    Offstage(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
